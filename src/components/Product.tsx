2d473a8b347a076b0df3f20f33ec46842a9702c1
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-22 21:45:24 +0500

    feat: create a Product component for the shop page

---
+++ src/components/Product.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import StarRating from "./StarRating";
+import { ProductType } from "../types/product.type";
+
+interface Props {
+  product: ProductType;
+}
+export default function Product({
+  product: { title, price, rating, image },
+}: Props) {
+  return (
+    <div>
+      <div className="w-full aspect-square relative rounded-2xl overflow-hidden  bg-product-background mb-4">
+        <Image
+          src={image}
+          fill
+          className="object-center object-contain p-5"
+          alt=""
+        />
+      </div>
+      <div className="space-y-1">
+        <h2 className="font-satoshi font-bold text-xl text-primary">{title}</h2>
+        <div className="flex justify-start items-center gap-4">
+          <StarRating rating={rating.rate} starSize={16} />
+          <span>
+            <span className="text-primary">{rating.rate}</span>/5{" "}
+            {`(${rating.count})`}
+          </span>
+        </div>
+        <div className="font-satoshi font-bold text-primary text-xl">
+          ${price}
+        </div>
+      </div>
+    </div>
+  );
+}
