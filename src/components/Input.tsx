d08e836357bbbb83791f55a8fd5783bdc57bfd66
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-22 23:34:05 +0500

    style: make Input responsive

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ export default function Input({ icon, className, ...props }: Props) {
     return (
       <input
         className={twMerge(
-          "py-3 px-5 bg-secondary text-secondary-foreground rounded-full w-full placeholder:text-body",
+          "py-2 md:py-3 px-3 md:px-5 bg-secondary text-secondary-foreground rounded-full w-full placeholder:text-body",
           className
         )}
         {...props}
@@ -18,7 +18,7 @@ export default function Input({ icon, className, ...props }: Props) {
   return (
     <div
       className={twMerge(
-        "flex py-3 px-5 text-secondary-foreground bg-secondary justify-start items-center rounded-full gap-2 w-full",
+        "flex py-2 md:py-3 px-3 md:px-5 text-secondary-foreground bg-secondary justify-start items-center rounded-full gap-2 w-full",
         className
       )}
     >
