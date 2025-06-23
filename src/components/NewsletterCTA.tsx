ab872023e349d0f19259c8a36c0649d71a4ffed8
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-23 15:13:02 +0500

    style: make NewsletterCTA responsive

--- src/components/NewsletterCTA.tsx
+++ src/components/NewsletterCTA.tsx
@@ -4,11 +4,11 @@ import Input from "./Input";
 
 export default function NewsletterCTA() {
   return (
-    <div className="bg-primary text-primary-foreground flex justify-between items-center py-8 px-10 rounded-xl">
-      <h4 className="font-integralcf font-bold text-primary-foreground text-4xl">
+    <div className="bg-primary gap-10 text-primary-foreground flex flex-col sm:flex-row justify-between items-center py-6 md:py-8 px-6 md:px-10 rounded-xl">
+      <h4 className="font-integralcf font-bold text-primary-foreground text-3xl md:text-4xl">
         Stay upto date about <br /> our latest offers
       </h4>
-      <form className="space-y-4 w-1/4">
+      <form className="space-y-4 w-full sm:w-1/2 md:w-1/3">
         <Input
           className="placeholder:text-body"
           icon={<TfiEmail />}
