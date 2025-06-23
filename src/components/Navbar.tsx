78c99444ea905bcfa3649ef714b0b716b548c22a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-23 20:49:24 +0500

    refactor: make Navbar clean

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,7 @@
 import Link from "@/components/Link";
-import Input from "./Input";
 import NextLink from "next/link";
-import { CiSearch } from "react-icons/ci";
-import Button from "./Button";
-import { FaCartShopping } from "react-icons/fa6";
+import MobileNavbarLinks from "./MobileNavbarLinks";
+import Searchbar from "./Searchbar";
 import ShoppingCart from "./ShoppingCart";
 
 const navlinks = [
@@ -14,30 +12,35 @@ const navlinks = [
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between py-3 border-b border-border">
+    <div className="flex items-center justify-start gap-5 py-3 relative">
       <NextLink
         href={"/"}
-        className="capitalize font-integralcf font-bold text-2xl text-primary mr-10"
+        className="capitalize font-integralcf font-bold text-xl leading-none md:text-2xl text-primary"
       >
         Trendy Shop
       </NextLink>
-      <nav>
-        <menu className="flex gap-4">
-          {navlinks.map((link) => (
-            <li key={link.id}>
-              <Link className="whitespace-nowrap" href={link.href}>
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </menu>
-      </nav>
-      <Input
-        className="bg-muted max-w-[400px]"
-        icon={<CiSearch />}
-        placeholder="Search for Products..."
-      />
-      <ShoppingCart />
+      <div className="flex justify-end md:justify-between items-center w-full">
+        <DesktopNavbarLinks />
+        <Searchbar />
+        <MobileNavbarLinks />
+        <ShoppingCart />
+      </div>
     </div>
+  );
+}
+
+function DesktopNavbarLinks() {
+  return (
+    <nav className="hidden md:block">
+      <menu className="flex gap-4">
+        {navlinks.map((link) => (
+          <li key={link.id}>
+            <Link className="whitespace-nowrap" href={link.href}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </menu>
+    </nav>
   );
 }
