42f6ff7e497ef4f6af215ada7a2f27980f589312
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-23 20:41:27 +0500

    refactor: remove links from products and show "no product found" message when there are no products

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -9,11 +9,13 @@ interface Props {
 export default function ProductGrid({ products }: Props) {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-5 gap-y-10">
-      {products.map((product) => (
-        <Link key={product.id} href={`/products/${product.id}`}>
-          <Product product={product} />
-        </Link>
-      ))}
+      {products.length ? (
+        products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
+      ) : (
+        <p className="text-xl">No Products found</p>
+      )}
     </div>
   );
 }
