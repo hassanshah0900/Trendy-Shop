68fdf3e010d341e8e1aac6648ef382b8a97526cb
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-25 15:00:01 +0500

    refactor: set up Tanstack Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import localFont from "next/font/local";
-import Navbar from "@/components/Navbar";
 import Container from "@/components/Container";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import NewsletterCTA from "@/components/NewsletterCTA";
 import Separator from "@/components/Separator";
 import { FilterContextProvider } from "@/contexts/FilterContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Toaster } from "sonner";
+import "./globals.css";
+import { queryClient } from "@/client";
 
 const satoshi = localFont({
   src: "../../public/fonts/satoshi-variable.ttf",
@@ -32,20 +34,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${intergralcf.variable} ${satoshi.variable}`}>
       <body className="overflow-x-hidden text-sm md:text-base font-satoshi text-body bg-background">
-        <FilterContextProvider>
-          <Container>
-            <Navbar />
-            <Separator />
-          </Container>
-          {children}
-          <div className="translate-y-1/3">
+        <QueryClientProvider client={queryClient}>
+          <FilterContextProvider>
             <Container>
-              <NewsletterCTA />
+              <Navbar />
+              <Separator />
             </Container>
-          </div>
-          <Footer />
-        </FilterContextProvider>
-        <Toaster />
+            {children}
+            <div className="translate-y-1/3">
+              <Container>
+                <NewsletterCTA />
+              </Container>
+            </div>
+            <Footer />
+          </FilterContextProvider>
+          <Toaster />
+        </QueryClientProvider>
       </body>
     </html>
   );
