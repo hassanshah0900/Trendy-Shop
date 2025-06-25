337247002da8f05871e924b44e53619c80d0fccb
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-25 15:01:18 +0500

    feat: fetch products in Product Grid using Tanstack Query

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,15 +1,50 @@
-import { ProductType } from "../types/product.type";
-import Product from "./Product";
+"use client";
 
-interface Props {
-  products: ProductType[];
+import { useQuery } from "@tanstack/react-query";
+import Product from "./Product";
+import { useFilters } from "@/contexts/FilterContext";
+import { filterProducts } from "@/lib/utils";
+export interface ProductType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: { rate: number; count: number };
 }
 
-export default function ProductGrid({ products }: Props) {
+export default function ProductGrid() {
+  const { filters } = useFilters();
+  const {
+    data: products,
+    isError,
+    error,
+    isLoading,
+  } = useQuery<ProductType[]>({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      if (!res.ok) {
+        throw new Error("An error has occured.");
+      }
+      return await res.json();
+    },
+  });
+
+  if (isLoading)
+    return (
+      <div className="w-full mt-40 flex justify-center items-center">
+        <div className="animate-spin w-10 h-10 border-2 border-r-border rounded-full"></div>
+      </div>
+    );
+  if (isError) return <div>{error.message}</div>;
+
+  const filteredProducts = filterProducts(products || [], filters);
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-5 gap-y-10">
-      {products.length ? (
-        products.map((product) => (
+      {filteredProducts.length ? (
+        filteredProducts.map((product) => (
           <Product key={product.id} product={product} />
         ))
       ) : (
